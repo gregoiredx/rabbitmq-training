2ea1f741dc831c14e2c40aa55d7f9fbd8d744ac7
Grégoire Deveaux <gregoire.deveaux@backmarket.com>
2022-06-13 16:46:25 +0200

    docker compose !

--- start.sh
+++ start.sh
@@ -8,23 +8,23 @@ chmod ag-rwx /var/lib/rabbitmq/.erlang.cookie
 
 rabbitmq-server &
 
-sleep 5
-rabbitmqctl add_user full_access test
-rabbitmqctl set_user_tags full_access "administrator"
-rabbitmqctl set_permissions -p "/" full_access ".*" ".*" ".*"
-rabbitmq-plugins enable rabbitmq_management
-rabbitmq-plugins enable rabbitmq_event_exchange
-rabbitmq-plugins enable rabbitmq_tracing
-rabbitmqctl trace_on
-
 if [[ "$NODE_TO_JOIN" == "" ]]
 then
-  echo "############# MAIN NODE STARTED $RABBITMQ_NODENAME"
+  echo "############# STARTING MAIN NODE $RABBITMQ_NODENAME"
+  sleep 5
+  rabbitmqctl add_user full_access test
+  rabbitmqctl set_user_tags full_access "administrator"
+  rabbitmqctl set_permissions -p "/" full_access ".*" ".*" ".*"
+  rabbitmq-plugins enable rabbitmq_management
+  rabbitmq-plugins enable rabbitmq_event_exchange
+  rabbitmq-plugins enable rabbitmq_tracing
+  rabbitmqctl trace_on
 else
   echo "############# NODE $RABBITMQ_NODENAME JOINING $NODE_TO_JOIN"
+  sleep 10
   rabbitmqctl stop_app
   rabbitmqctl reset
-  rabbitmqctl join_cluster rabbit@server-1
+  rabbitmqctl join_cluster $NODE_TO_JOIN
   rabbitmqctl start_app
 fi
 
