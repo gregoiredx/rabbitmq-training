6bbd9166825ab98dd2beb492037b58b8812de25e
Grégoire Deveaux <gregoire.deveaux@backmarket.com>
2022-06-13 11:56:11 +0200

    traces and events

---
+++ bind_events.py
@@ -0,0 +1,26 @@
+from lib import channel, connection
+
+channel.queue_declare(
+    queue="events_queue", durable=True, exclusive=False, auto_delete=False
+)
+
+channel.queue_declare(
+    queue="traces_queue", durable=True, exclusive=False, auto_delete=False
+)
+
+channel.queue_declare(
+    queue="events_user_auth_queue", durable=True, exclusive=False, auto_delete=False
+)
+
+# Show all system events (binding, connection, authentication, ...)
+channel.queue_bind(exchange="amq.rabbitmq.event", queue="events_queue", routing_key="#")
+# Show all published messages
+channel.queue_bind(exchange="amq.rabbitmq.trace", queue="traces_queue", routing_key="#")
+# Show only user authentication events
+channel.queue_bind(
+    exchange="amq.rabbitmq.event",
+    queue="events_user_auth_queue",
+    routing_key="user.authentication.*",
+)
+
+connection.close()
--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -m
 rabbitmq-server &
-sleep 5
 rabbitmqctl await_startup
+sleep 5
 rabbitmqctl add_user full_access test
 rabbitmqctl set_user_tags full_access "administrator"
 rabbitmqctl set_permissions -p "/" full_access ".*" ".*" ".*"
 rabbitmq-plugins enable rabbitmq_management
+rabbitmq-plugins enable rabbitmq_event_exchange
+rabbitmq-plugins enable rabbitmq_tracing
+rabbitmqctl trace_on
 fg %1
