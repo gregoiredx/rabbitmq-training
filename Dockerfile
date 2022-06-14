c2eb5f1705430e98c371229150ff90d496a47055
Grégoire Deveaux <gregoire.deveaux@backmarket.com>
2022-06-14 09:05:58 +0200

    config file

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ EXPOSE 5672
 EXPOSE 15671 15672
 ADD start.sh "/start.sh"
 RUN chmod +x /start.sh
+ADD rabbitmq.conf /etc/rabbitmq/rabbitmq.conf
 CMD "/start.sh"
--- start.sh
+++ start.sh
@@ -8,24 +8,17 @@ chmod ag-rwx /var/lib/rabbitmq/.erlang.cookie
 
 rabbitmq-server &
 
-if [[ "$NODE_TO_JOIN" == "" ]]
+sleep 10
+if [[ "$RUN_CMDS" == "true" ]]
 then
-  echo "############# STARTING MAIN NODE $RABBITMQ_NODENAME"
-  sleep 5
-  rabbitmqctl add_user full_access test
+  rabbitmqctl await_startup
+  rabbitmqctl add_user full_access test || true
   rabbitmqctl set_user_tags full_access "administrator"
   rabbitmqctl set_permissions -p "/" full_access ".*" ".*" ".*"
   rabbitmq-plugins enable rabbitmq_management
   rabbitmq-plugins enable rabbitmq_event_exchange
   rabbitmq-plugins enable rabbitmq_tracing
   rabbitmqctl trace_on
-else
-  echo "############# NODE $RABBITMQ_NODENAME JOINING $NODE_TO_JOIN"
-  sleep 10
-  rabbitmqctl stop_app
-  rabbitmqctl reset
-  rabbitmqctl join_cluster "$NODE_TO_JOIN"
-  rabbitmqctl start_app
 fi
 
 fg %1
