193dd57a926b3efa84256bd96e8fb97299adebc9
Grégoire Deveaux <gregoire.deveaux@backmarket.com>
2022-06-14 11:57:39 +0200

    use default user

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
-FROM rabbitmq:latest
+FROM rabbitmq:3.10.5-management
 EXPOSE 5672
-EXPOSE 15671 15672
 ADD start.sh "/start.sh"
 RUN chmod +x /start.sh
 ADD rabbitmq.conf /etc/rabbitmq/rabbitmq.conf
--- start.sh
+++ start.sh
@@ -12,10 +12,6 @@ sleep 10
 if [[ "$RUN_CMDS" == "true" ]]
 then
   rabbitmqctl await_startup
-  rabbitmqctl add_user full_access test || true
-  rabbitmqctl set_user_tags full_access "administrator"
-  rabbitmqctl set_permissions -p "/" full_access ".*" ".*" ".*"
-  rabbitmq-plugins enable rabbitmq_management
   rabbitmq-plugins enable rabbitmq_event_exchange
   rabbitmq-plugins enable rabbitmq_tracing
   rabbitmqctl trace_on
